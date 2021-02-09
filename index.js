0d2cd4eaf30a58c1a9a62000a6f2ef56baa4210c
Leo Carvalho <carvalho.csleo@gmail.com>
2021-02-09 03:19:25 -0300

    Install and configure morgan

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
 const port = Number(process.env.PORT);
+
+const accessLogStream = rfs.createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: path.join(__dirname, 'logs')
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(routes);
 app.listen(port);
