796bbf5163b22e3096001833c70e0b7e7ad8934f
Leo Carvalho <carvalho.csleo@gmail.com>
2021-02-10 02:38:03 -0300

    Remove control of instances and extract function

--- controllers/TweetsController.js
+++ controllers/TweetsController.js
@@ -1,19 +1,14 @@
 'use strict';
 const client = require('../services/client');
+const isReply = require('../utils/isReply');
 
 const query = {
   track: 'Martinho Lutero,#ReformaProtestante,#ProtestantReform,#95teses,#5solas,#Reforma504Anos,-is:retweet',
 };
 class TweetsController {
   static stream;
-  static counter = 0;
 
   getTweets(req, res) {
-    TweetsController.counter++;
-    if(TweetsController.counter > 1) {
-      return res.status(420).send('Not permitted: there is another instance doing the same thing');
-    }
-
     TweetsController.stream = client.stream('statuses/filter', query)
     TweetsController.stream
         .on("start", response => {
@@ -21,9 +16,9 @@ class TweetsController {
           res.status(200).json({message: "Inicializado corretamente", response})
         })
         .on("data", tweet => {
-          const isReply = TweetsController.isReply(tweet);
+          const isTweetReply = isReply(tweet);
 
-          if(!isReply) {
+          if(!isTweetReply) {
             console.log(`Tweet de @${tweet.user.screen_name}: "${tweet.text}".`)
             TweetsController.retweet(tweet);
           }
@@ -55,20 +50,6 @@ class TweetsController {
       console.error(error)
       //throw new Error(error)
      }
-  }
-
-  static isReply(tweet) {
-    if (tweet.retweeted_status
-      || tweet.in_reply_to_status_id
-      || tweet.in_reply_to_status_id_str
-      || tweet.in_reply_to_user_id
-      || tweet.in_reply_to_user_id_str
-      || tweet.in_reply_to_screen_name
-      || tweet.retweeted_status
-      || tweet.delete) {
-        return true
-      }
-    return false
   }
 }
 
