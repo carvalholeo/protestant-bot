ddbec2a977e4fc640ae7583af32679ef42b48984
Leo Carvalho <carvalho.csleo@gmail.com>
2021-02-11 00:50:29 -0300

    Fix code style guide and create JSDoc

--- logs/logger.js
+++ logs/logger.js
@@ -1,21 +1,32 @@
-'use strict';
-
 const path = require('path');
 const fs = require('fs');
 
+
+/**
+ * Function to log message from some point of the system.
+ *
+ * @param {string} logName Name of the log where you gonna store informations.
+ * @param {string} message Message that will be stored in the log.
+ */
 function logger(logName, message) {
+  const dateTime = new Date().toLocaleString('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
+  });
   try {
     const logFile = path.resolve(__dirname, `${logName}.log`);
-    const dateTime = new Date().toLocaleString("pt-BR", { timeZone: 'America/Sao_Paulo' });
     const additionalMessage = `${dateTime}: ${message}\n`;
 
-    fs.appendFileSync(logFile, additionalMessage, { encoding: 'utf-8', flag: 'a' });
+    fs.appendFileSync(logFile, additionalMessage, {
+      encoding: 'utf-8',
+      flag: 'a',
+    });
   } catch (error) {
-    const logFile = path.resolve(__dirname, `kernelPanic.log`);
-    const dateTime = new Date().toLocaleString("pt-BR", { timeZone: 'America/Sao_Paulo' });
+    const logFile = path.resolve(__dirname, 'kernelPanic.log');
     const additionalMessage = `${dateTime}: ERROR: ${error.message}\n`;
-
-    fs.appendFileSync(logFile, additionalMessage, { encoding: 'utf-8', flag: 'a' });
+    fs.appendFileSync(logFile, additionalMessage, {
+      encoding: 'utf-8',
+      flag: 'a',
+    });
 
     process.exit(255);
   }
--- utils/makeError.js
+++ utils/makeError.js
@@ -5,9 +5,17 @@ const logger = require('../logs/logger');
 
 const stream = new Stream();
 
+
+/**
+ * Function called when some point needs to stop stream.
+ * After close connection, the problem is logged and system exited.
+ * @param {string} error Error message emitted from the origin.
+ * @param {string} origin Original source of the error.
+ */
 function makeError(error, origin) {
   process.nextTick(() => stream.killInstance());
-  const message = `An error was produced by ${origin}. The message emitted was '${error}'.`;
+  const message = `An error was produced by ${origin}.
+  The message emitted was '${error}'.`;
 
   logger('error', message);
 }
