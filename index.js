6992f3c66af198d9ff96ee3131ae25ff4b385882
Leo Carvalho <carvalho.csleo@gmail.com>
2021-04-08 01:01:00 -0300

    Fix bug at CORS pre-flight

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const path = require('path');
 const routes = require('./routes');
 
 const app = express();
+const origin = process.env.FRONTEND_URL ?? 'http://localhost:3000';
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: origin,
   preflightContinue: true,
   optionsSuccessStatus: 200,
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
