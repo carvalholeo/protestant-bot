9c55af393b4534b92e1e4414308a754b662ff0b4
Leo Carvalho <carvalho.csleo@gmail.com>
2021-04-09 00:12:32 -0300

    Create new methods on retweet log

--- models/RetweetLog.js
+++ models/RetweetLog.js
@@ -1,4 +1,9 @@
 const BaseLogModel = require('./BaseLog');
+const ErrorLog = require('./ErrorLog');
+const logger = require('../logs/logger');
+
+const initialLimit = 100;
+const initialOffset = 100;
 /**
  * Class to log all retweets done by the application
  * @class RetweetLog
@@ -12,6 +17,70 @@ class RetweetLog extends BaseLogModel {
    */
   constructor() {
     super('retweet');
+  }
+
+  /**
+   * Method to retrieve all rewteets done on bot.
+   * @param {number} page Page number of data to be retrivied.
+   * @return {Array<JSON> | JSON} Return an array on success by retrieving.
+   */
+  async getAllRetweets(page = 1) {
+    try {
+      const limit = page === 1 ? initialLimit : page * initialLimit;
+      const offset = initialOffset * (page - 1);
+
+      return await this._connection
+          .where({was_undone: false})
+          .limit(limit)
+          .offset(offset)
+          .select('*');
+    } catch (error) {
+      const message = `An error occurred on retrivieing rewteets.
+      Message generated: ${error}`;
+      logger('error', message, new ErrorLog());
+    }
+  }
+
+  /**
+   * Method to retrieve all rewteets undone on bot.
+   * @param {number} page Page number of data to be retrivied.
+   * @return {Array<JSON> | JSON} Return an array on success by retrieving.
+   */
+  async getAllRetweetsUndone(page = 1) {
+    try {
+      const limit = page === 1 ? initialLimit : page * initialLimit;
+      const offset = initialOffset * (page - 1);
+
+      return await this._connection
+          .where({was_undone: true})
+          .limit(limit)
+          .offset(offset)
+          .select('*');
+    } catch (error) {
+      const message = `An error occurred on retrivieing rewteets undone.
+      Message generated: ${error}`;
+      logger('error', message, new ErrorLog());
+    }
+  }
+  /**
+   * Undo retweets and add a comment about this decision.
+   * @param {string} tweetId Tweet to be undone on Twitter
+   * @param {string} comment Comment about decison of retweet undone
+   */
+  async undoRetweet(tweetId, comment = '') {
+    try {
+      await this._connection
+          .where({tweet_id: tweetId})
+          .update({
+            comment: comment === '' ? null : comment,
+            was_undone: true,
+            updated_at: this.dateTime,
+          });
+    } catch (error) {
+      const message = `An error occurred on retrivieing rewteets undone.
+      Message generated: ${error}`;
+      logger('error', message, new ErrorLog());
+    }
   }
 }
 
