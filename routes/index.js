1e3658fe2834e5b963eb352e9ef314569342f1f4
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-08 22:13:25 -0300

    Map retweets routes and JWT middlewares/validations

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
+'use strict';
+// @ts-check
+
 const {Router} = require('express');
 
+const validatorMiddleware = require('../middlewares/validatorMiddleware');
+const authorizationMiddleware =
+  require('../middlewares/authorizarionMiddleware');
+
+const tokenValidator = require('../validators/privateApi/tokenValidator');
+
 const blocklist = require('./blocklist');
+const tweets = require('./tweets');
 
 // eslint-disable-next-line new-cap
 const routes = Router();
 
 routes.use('/blocklist', blocklist);
+
+routes.use(tokenValidator);
+routes.use(validatorMiddleware);
+
+routes.use(authorizationMiddleware);
+
+routes.use('/tweets', tweets);
 
 module.exports = routes;
