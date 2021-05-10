b18ee89e8d3d6f1b7b90a8d2ad26a171e6afb2b7
Leo Carvalho <carvalho.csleo@gmail.com>
2021-05-10 01:38:38 -0300

    Reorganize structure of files

--- index.js
+++ index.js
@@ -14,14 +14,10 @@ const path = require('path');
 const hpp = require('hpp');
 
 const generateSecretToJWT = require('./utils/generateSecretToJWT');
-
 const tooBusyMiddleware = require('./middlewares/tooBusyMiddleware');
-
-process.env.JWT_SECRET = generateSecretToJWT();
-
 const routes = require('./routes');
 
-const app = express();
+process.env.JWT_SECRET = generateSecretToJWT();
 
 const origin = process.env.FRONTEND_URL ?? 'http://localhost:3000';
 const port = Number(process.env.PORT) ?? 3000;
@@ -30,24 +26,22 @@ const corsOptions = {
   preflightContinue: true,
   optionsSuccessStatus: 200,
 };
-
-app.use(helmet());
-app.use(cors(corsOptions));
-app.options('*', cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-app.use(hpp());
-
 const accessLogStream = rfs.createStream('access.log', {
   interval: '1d', // rotate daily
   path: path.join(__dirname, 'logs'),
 });
 
-app.use(morgan('combined', {stream: accessLogStream}));
+const app = express();
 
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(morgan('combined', {stream: accessLogStream}));
+app.use(hpp());
 app.use(tooBusyMiddleware);
 
+app.options('*', cors());
 app.use('/api', routes);
 app.listen(port);
 
