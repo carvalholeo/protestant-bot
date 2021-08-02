1a66b2b09dd1c8c05cde06695b09d888ca43f0a5
Leo Carvalho <carvalho.csleo@gmail.com>
2021-08-02 02:08:52 -0300

    Implement error handler Implement cookie parser Export file as default

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 
 const generateSecretToJWT = require('./utils/generateSecretToJWT');
 const routes = require('./routes');
@@ -27,12 +29,14 @@ const corsOptions = {
 
 const app = express();
 
+app.use(httpLogger);
 app.use(helmet());
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(httpLogger);
+app.use(cookieParser());
 app.use(hpp());
+
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV !== 'test') {
   const bouncerLimiter = require('./middlewares/bouncerLimiter');
@@ -41,5 +45,22 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.options('*', cors());
 app.use('/api', routes);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({message: err.message});
+});
+
+
 app.listen(port);
 
+module.exports = app;
