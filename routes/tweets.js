f2b1f50ab7e5ca083623c0de89512492ffb04b3b
Leo Carvalho <carvalho.csleo@gmail.com>
2021-08-07 20:58:24 -0300

    Remove unusual methods

--- routes/tweets.js
+++ routes/tweets.js
@@ -5,7 +5,6 @@ const {Router} = require('express');
 
 const validatorMiddleware = require('../middlewares/validatorMiddleware');
 const TweetsController = require('../controllers/TweetsController');
-const DefaultController = require('../controllers/DefaultController');
 
 const verifyLogoutMiddleware = require('../middlewares/verifyLogoutMiddleware');
 const authorizationMiddleware =
@@ -26,11 +25,6 @@ tweets.use(authorizationMiddleware);
 tweets.use(verifyLogoutMiddleware);
 
 tweets
-    .post('/retweets', DefaultController.methodNotAllowed)
-    .put('/retweets', DefaultController.methodNotAllowed)
-    .patch('/retweets', DefaultController.methodNotAllowed)
-    .head('/retweets', DefaultController.methodNotAllowed)
-    .delete('/retweets', DefaultController.methodNotAllowed)
     .get('/retweets',
         listRetweets,
         validatorMiddleware,
@@ -38,11 +32,6 @@ tweets
     );
 
 tweets
-    .get('/:tweetId', DefaultController.methodNotAllowed)
-    .post('/:tweetId', DefaultController.methodNotAllowed)
-    .put('/:tweetId', DefaultController.methodNotAllowed)
-    .patch('/:tweetId', DefaultController.methodNotAllowed)
-    .head('/:tweetId', DefaultController.methodNotAllowed)
     .delete('/:tweetId',
         undoRetweets,
         validatorMiddleware,
