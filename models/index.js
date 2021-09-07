9fd4624c05fb5a54fac927aae3c34b436e94c464
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-07 18:41:51 -0300

    Include Configuration model to the index file

--- models/index.js
+++ models/index.js
@@ -1,21 +1,23 @@
 // @ts-check
 
 const AccessLog = require('./AccessLog');
+const Blocklist = require('./Blocklist');
+const Configuration = require('./Configuration');
 const ErrorLog = require('./ErrorLog');
 const KernelPanicLog = require('./KernelPanicLog');
+const RateLimit = require('./RateLimit');
 const RetweetLog = require('./RetweetLog');
 const TweetQueue = require('./TweetQueue');
-const RateLimit = require('./RateLimit');
-const Blocklist = require('./Blocklist');
 const User = require('./User');
 
 module.exports = {
   AccessLog,
+  Blocklist,
+  Configuration,
   ErrorLog,
   KernelPanicLog,
+  RateLimit,
   RetweetLog,
   TweetQueue,
-  RateLimit,
-  Blocklist,
   User,
 };
