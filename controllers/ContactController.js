c1475248f2850ad266653059df7dfb0d47a2937e
Leo Carvalho <carvalho.csleo@gmail.com>
2021-09-13 19:03:35 -0300

    Implement email service at controller

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -7,6 +7,7 @@ const {
   AccessLog,
 } = require('../models');
 const logger = require('../logs/logger');
+const {sendMail} = require('../services/emails');
 
 const ContactController = {
   create: async (request, response) => {
@@ -21,12 +22,15 @@ const ContactController = {
 created at database.`;
 
       await logger('access', messageToUser, new AccessLog());
-      return response
+      response
           .status(201)
           .json({messageToUser});
+
+      // @ts-ignore
+      await sendMail({name, email, twitter, message});
     } catch (error) {
       console.error(error);
-      const message = `Error on block user.
+      const message = `Error on register contact at database.
       Reason: ${error}`;
       await logger('error', message, new ErrorLog());
       return response
