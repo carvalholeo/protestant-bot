f0596fa1442f5a69433c59aa9214dceacd9e9ba5
Leonardo Carvalho <carvalho.csleo@gmail.com>
2021-11-07 01:27:05 -0300

    Migrate ConfigurationService from Knex to Sequelize

--- src/services/models/Configuration.ts
+++ src/services/models/Configuration.ts
@@ -1,42 +1,32 @@
-import BaseModel from './Base';
+import models from '../../db/models';
 import ErrorLog from './ErrorLog';
 import AccessLog from './AccessLog';
 import logger from '../../logs/logger';
-
-import ConfigurationModel from '../../interfaces/typeDefinitions/ConfigurationModel';
 
 /**
  * Handle with app configurations
  * tweets retweet by the bot.
  * @class Configuration
- * @extends BaseModel
  */
-class Configuration extends BaseModel {
-  /**
-   * On instantiate, pass to the construtor of the base class
-   * the name of the table.
-   */
-  constructor() {
-    super('configuration');
-  }
-
+class Configuration {
   /**
    * Get the configuration of the bot
-   * @param key - the key of the configuration
-   * @return A string with configurated value. If any configuration were registered, so it returns undefined.
-   * @memberof Configuration
+   * @param {string} key - the key of the configuration
+   * @return {Promise<string | undefined>} A string with configurated value.
+   * If any configuration were registered, so it returns undefined.
    */
   async getConfig(key: string): Promise<string | undefined> {
     try {
-      const config: ConfigurationModel = await this._connection
-        .where('key', key)
-        .select('*')
-        .first();
+      const config = await models.Configurations.findOne({
+        where: {
+          key,
+        },
+      });
 
-      if (config) {
-        return config.value;
+      if (config === null) {
+        return undefined;
       }
-      return undefined;
+      return config.value;
     } catch (error: any) {
       const message = `Error from Configuration class, method getConfig.
       Message catched: ${error.message}.
@@ -47,19 +37,19 @@ class Configuration extends BaseModel {
 
   /**
    * Get the configuration of the bot
-   * @param key - the key of the configuration
-   * @param value - the value of the configuration
-   * @memberof Configuration
+   * @param {string} key - the key of the configuration
+   * @param {string} value - the value of the configuration
    */
   async setConfig(key: string, value: string): Promise<void> {
     try {
       const data = {
         value,
-        updatedAt: this.dateTime,
       };
-      await this._connection
-        .where('key', key)
-        .update(data);
+      await models.Configurations.update(data, {
+        where: {
+          key,
+        },
+      });
 
       const message = `Config ${key} updated with value ${value}.`;
       logger('access', message, new AccessLog());
@@ -73,20 +63,17 @@ class Configuration extends BaseModel {
 
   /**
    * Create a new configuration of the bot
-   * @param key - the key of the configuration
-   * @param value - the value of the configuration
-   * @memberof Configuration
+   * @param {string} key - the key of the configuration
+   * @param {string} value - the value of the configuration
    */
   async createConfig(key: string, value: string): Promise<void> {
     try {
       const data = {
         key,
         value,
-        createdAt: this.dateTime,
-        updatedAt: this.dateTime,
       };
-      await this._connection
-        .insert(data);
+
+      await models.Configurations.create(data);
 
       const message = `Config ${key} created with value ${value}.`;
       logger('access', message, new AccessLog());
@@ -99,14 +86,15 @@ class Configuration extends BaseModel {
   }
   /**
    * Delete a configuration of the bot
-   * @param key - the key of the configuration
-   * @memberof Configuration
+   * @param {string} key - the key of the configuration
    */
   async deleteConfig(key: string): Promise<void> {
     try {
-      await this._connection
-        .where('key', key)
-        .delete();
+      await models.Configurations.delete({
+        where: {
+          key,
+        },
+      });
 
       const message = `Config ${key} successfully.`;
       logger('access', message, new AccessLog());
