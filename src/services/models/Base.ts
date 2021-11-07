7fd1ef50446edfe0ac40bd8c86283f0e1162c38a
Leonardo Carvalho <carvalho.csleo@gmail.com>
2021-11-07 01:18:27 -0300

    Migrate logs services from Knex to Sequelize

--- src/services/models/Base.ts
+++ src/services/models/Base.ts
@@ -1,5 +1,3 @@
-import { Knex, knex } from 'knex';
-import connection from '../../database/connection';
 import logger from '../../logs/logger';
 
 /**
@@ -7,22 +5,6 @@ import logger from '../../logs/logger';
  * Shouldn't be instantiated.
  */
 abstract class Base {
-  public dateTime: Date = new Date();
-  protected _connection: any;
-  /**
-   * @param {string} nameOfModel Name of the model to connect
-   */
-  constructor(nameOfModel: string) {
-    try {
-      if (typeof(nameOfModel) === 'undefined' || nameOfModel === null) {
-        throw new ReferenceError('You must to provide a table to be handled.');
-      }
-      this.dateTime = new Date();
-      this._connection = connection(nameOfModel);
-    } catch (error: any) {
-      this.emergencyLog(error.message);
-    }
-  }
   /**
    * Use this method only when database logging is unavailable.
    * @param {string} message Message to be stored at Kernel Panic log.
