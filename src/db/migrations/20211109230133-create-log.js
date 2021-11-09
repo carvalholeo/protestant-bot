59d52a106794da4205150163aef29b09900fcdc2
Leonardo Carvalho <carvalho.csleo@gmail.com>
2021-11-09 20:57:30 -0300

    Create model and migration to a unified system log

---
+++ src/db/migrations/20211109230133-create-log.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Logs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      level: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+        defaultValue: 'debug',
+      },
+      emmiter: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Logs');
+  },
+};
