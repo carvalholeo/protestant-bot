975c9d0aa633337432e60a53f9b4db316c17106b
Leonardo Carvalho <carvalho.csleo@gmail.com>
2021-11-12 00:03:08 -0300

    Delete unused tables

---
+++ src/db/migrations/20211112025131-delete-kernelpaniclog-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('KernelPanicLog');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('KernelPanicLog', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+};
