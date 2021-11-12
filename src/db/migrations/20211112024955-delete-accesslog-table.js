28c7db2a2830209249b93fc0c8ed25fcb9c2a8d1
Leonardo Carvalho <carvalho.csleo@gmail.com>
2021-11-12 00:03:08 -0300

    Delete unused tables

---
+++ src/db/migrations/20211112024955-delete-accesslog-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('AccessLog');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('AccessLog', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      message: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+};
