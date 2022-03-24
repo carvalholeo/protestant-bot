a47aea57ed021179cf631d753c6f3bc0486ce485
Léo Carvalho <carvalho.csleo@gmail.com>
2022-03-24 01:54:09 -0300

    Fix Lint errors

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ interface Error {
   status: number;
 }
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
