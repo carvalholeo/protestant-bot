0fce3779830b94103d83d4283bfd2f81318130dc
Léo Carvalho <carvalho.csleo@gmail.com>
2022-05-04 22:32:05 -0300

    Refactor format into transports

--- src/services/logs/logger.ts
+++ src/services/logs/logger.ts
@@ -3,18 +3,27 @@ import { createLogger, config, transports, format } from 'winston';
 import 'winston-daily-rotate-file';
 import { DailyRotateFileTransportOptions } from 'winston-daily-rotate-file';
 
+const customFormat = format.combine(
+  format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+  format.align(),
+  format.printf(i => `${i.level}: ${[i.timestamp]}: ${i.message}`)
+)
+
 const consoleOptions: transports.ConsoleTransportOptions = {
   consoleWarnLevels: ['warn', 'debug', 'trace'],
+  format: customFormat,
   handleExceptions: true,
   handleRejections: true,
   level: 'debug',
   stderrLevels: ['error'],
 };
 
+
 const dailyRotateOptions: DailyRotateFileTransportOptions = {
   datePattern: 'YYYY-MM-DD',
   dirname: resolve(__dirname, '..', '..', 'logs'),
   filename: 'server.log',
+  format: customFormat,
   level: 'info',
   maxFiles: '366d',
   maxSize: '20m',
@@ -23,11 +32,6 @@ const dailyRotateOptions: DailyRotateFileTransportOptions = {
 
 const logger = createLogger({
   exitOnError: true,
-  format: format.combine(
-    format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
-    format.align(),
-    format.printf(i => `${i.level}: ${[i.timestamp]}: ${i.message}`)
-  ),
   levels: config.npm.levels,
   transports: [
     new transports.Console(consoleOptions),
