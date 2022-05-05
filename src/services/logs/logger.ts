ac180400ce58d4bace685aab533539797d3ecfcb
Léo Carvalho <carvalho.csleo@gmail.com>
2022-05-05 01:00:44 -0300

    Separate different formats for Console and File Colorize format message for Console Include automatic minimum level for production log file Automatic Recreation of log file if it were deleted Automatic handle for rejections and exceptions

--- src/services/logs/logger.ts
+++ src/services/logs/logger.ts
@@ -3,15 +3,22 @@ import { createLogger, config, transports, format } from 'winston';
 import 'winston-daily-rotate-file';
 import { DailyRotateFileTransportOptions } from 'winston-daily-rotate-file';
 
-const customFormat = format.combine(
+const textFormat = format.combine(
   format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
   format.align(),
-  format.printf(i => `${i.level}: ${[i.timestamp]}: ${i.message}`)
-)
+  format.printf(i => `${i.level}: ${i.timestamp}: ${i.message}`),
+);
+
+const consoleFormat = format.combine(
+  format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+  format.align(),
+  format.colorize(),
+  format.printf(i => `${i.level}: ${i.timestamp}: ${i.message}`),
+);
 
 const consoleOptions: transports.ConsoleTransportOptions = {
   consoleWarnLevels: ['warn', 'debug', 'trace'],
-  format: customFormat,
+  format: consoleFormat,
   handleExceptions: true,
   handleRejections: true,
   level: 'debug',
@@ -24,11 +31,14 @@ const dailyRotateOptions: DailyRotateFileTransportOptions = {
   dirname: resolve(__dirname, '..', '..', 'logs', 'app'),
   extension: '.log',
   filename: 'server.%DATE%',
-  format: customFormat,
-  level: 'info',
+  format: textFormat,
+  handleExceptions: true,
+  handleRejections: true,
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
   maxFiles: '366d',
   maxSize: '20m',
   zippedArchive: true,
+  watchLog: true,
 };
 
 const logger = createLogger({
