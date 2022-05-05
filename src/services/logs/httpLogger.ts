1cd4ff9fa7971142e8db23062856ca02887e9d7e
Léo Carvalho <carvalho.csleo@gmail.com>
2022-05-05 16:16:36 -0300

    Use unique identifier into HTTP logger

--- src/services/logs/httpLogger.ts
+++ src/services/logs/httpLogger.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import morgan from 'morgan';
 import { createStream } from 'rotating-file-stream';
+import {Request, Response} from 'express'
 
 const accessLogStream = createStream('access.log', {
   interval: '1d',
@@ -8,9 +9,22 @@ const accessLogStream = createStream('access.log', {
   maxSize: '5M',
   path: path.join(__dirname, '..', '..', 'logs', 'http'),
   teeToStdout: true,
-})
+});
 
-const httpLogger = morgan('combined', {
+const httpLogger = morgan(function(tokens, req: Request, res: Response) {
+  console.log(tokens)
+  return [
+    tokens['remote-addr'](req, res), '-',
+    tokens['remote-user'](req, res), '-',
+    `[${tokens.date(req, res, 'clf')}]`,
+    `"${tokens.method(req, res)} ${tokens.url(req, res)} HTTP/${tokens['http-version'](req, res)}"`,
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens.referrer(req, res),
+    `"${tokens['user-agent'](req, res)}"`, '-',
+    `"${req.app.get('uniqueIdentifier')}"`
+  ].join(' ');
+}, {
   stream: accessLogStream,
 });
 
