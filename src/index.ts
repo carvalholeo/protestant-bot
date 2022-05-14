8504d82dcfa72ed133bf0387c3c0cbe85fdd43f8
Léo Carvalho <carvalho.csleo@gmail.com>
2022-05-13 22:08:52 -0300

    Recreate cookies for app

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express, { json, urlencoded } from 'express';
 import helmet from 'helmet';
 import cors, { CorsOptions } from 'cors';
 import hpp from 'hpp';
+import cookieParser from 'cookie-parser';
 
 import routes from './routes';
 import httpLogger from './utils/logs/httpLogger';
@@ -19,6 +20,12 @@ const corsOptions: CorsOptions = {
   optionsSuccessStatus: 200,
 };
 
+const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+};
+
+// deepcode ignore UseCsurfForExpress: Csurf doesn't work with this app
 const app = express();
 const corsExecution = cors(corsOptions);
 
@@ -30,6 +37,12 @@ app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(hpp());
 
+app.use(
+  cookieParser(
+    process.env.JWT_SECRET,
+    COOKIE_OPTIONS as cookieParser.CookieParseOptions
+  )
+);
 app.use(corsExecution);
 
 app.options('*', corsExecution);
